177d77c850ac5b6f6217dc395d912fdb97ab4da6
katherynbuble <59714566+katherynbuble@users.noreply.github.com>
2020-01-22 16:47:57 -0800

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,32 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#by Victor Unda
 
-# Learning python:
-# https://www.python.org/
-
-import csv          # Lib/csv.py ...file
-import itertools    #function for an efficient looping
-import codecs
+import csv
+import docx
 import string
 
-with open("Citrange.txt", "r") as in_file:
+name = "Cranberry"
+in_file_ext = ".docx"
+out_file_ext = ".csv"
+input_name = name + in_file_ext
+d = docx.Document(input_name)
+full_text = []
 
-    linee = str(in_file.read())
-    cropline = str.replace(linee, 'Origin:', '<b>Origin:</b>')
-    cropline2 = str.replace(cropline, 'Fruit:', '<b>Fruit:</b>')
-    cropline3 = str.replace(cropline2, 'Tree:', '<b>Tree:</b>')
+for p in d.paragraphs:
+    line = p.text
+    full_text.append(line)
 
-    lines = cropline3.splitlines()     #returns a list with all the lines in string, including the line breaks
+t = '\n'.join(full_text)
+key_words = [ 'Origin:', 'Fruit:', 'Tree:', 'Plant:', 'Bush:', 'Vine:']
+for word in key_words:
+    if word in t:
+        t = t.replace(word, '<b>'+word+'</b>')
 
-    stripped = [line.split('. ', 1)for line in lines ] #split, first ". "
+lines = t.splitlines()
+# omit the first two lines with crop name and comment
+split_lines = [line.split('. ', 1) for line in lines[2:len(lines)]]
+output_name = name+out_file_ext
 
-    grouped = itertools.izip(*[stripped]*1) #Functions creating iterators for efficient (izip) looping
-
-    with open('Citrange.csv', 'w') as out_file: # this is my output file
-
-        nrsp10_w = csv.writer(out_file) # return object responsible for converting nrsp10 data into delimited strings.
-        nrsp10_w.writerow(('Crop', 'Description')) # write the row paremeter to the writer's nrsp10 data file object.
-
-        for group in grouped: # loop
-            nrsp10_w.writerows(group)
+with open(output_name, 'w', encoding='utf-8-sig') as f_out:
+    # return object for converting data into delimited strings.
+    obj_w = csv.writer(f_out) 
+    # write the column headers/fields to the writer data file object.
+    obj_w.writerow(('id', 'title', 'body', 'langcode', 'field_mlfruitandnut_crop', 'field_mlfruitandnut_cultivar', 
+                    'field_mlfruitandnut_fruit', 'field_mlfruitandnut_origin', 'field_mlfruitandnut_tree', 'field_link_the_site')) 
+   
+    # max = 5
+    count = 1
+    for line in split_lines:
+        id = str(count)
+        count = count + 1
+        title = line[0]
+        body = line[1]
+        print(type(body))
+        print(body)
+        langcode = "" 
+        
+        field_mlfruitandnut_crop = ""
+        field_mlfruitandnut_cultivar = ""
+        field_link_the_site = ""
+        if body.startswith('See '):
+            field_mlfruitandnut_crop = lines[0].title()
+            field_mlfruitandnut_cultivar =  title
+            field_link_the_site = (body.strip('.')).lstrip('See ') + " (" + title + ")"
+        else:
+            field_mlfruitandnut_crop = lines[0].title()
+            field_mlfruitandnut_cultivar =  title
+     
+            field_mlfruitandnut_fruit = "" 
+        field_mlfruitandnut_origin = ""
+        field_mlfruitandnut_tree = "" 
+        tline = [id, title, body, langcode, field_mlfruitandnut_crop, field_mlfruitandnut_cultivar, field_mlfruitandnut_fruit, 
+                 field_mlfruitandnut_origin, field_mlfruitandnut_tree, field_link_the_site]
+        t = () + (tline,)
+        obj_w.writerows(t)
